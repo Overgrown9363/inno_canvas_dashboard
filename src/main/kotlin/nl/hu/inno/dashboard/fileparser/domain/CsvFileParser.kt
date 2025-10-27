d35811d912e447a5a55e386b5e2ebc8e7519cab5
Patrick <patrick.damkat@student.hu.nl>
2025-10-27 17:03:45 +0100

    add initial CsvFileParser.kt

---
+++ src/main/kotlin/nl/hu/inno/dashboard/fileparser/domain/CsvFileParser.kt
@@ -0,0 +1,15 @@
+package nl.hu.inno.dashboard.fileparser.domain
+
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+
+@Component
+class CsvFileParser : FileParser {
+    override fun supports(file: MultipartFile): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun parse(file: MultipartFile): List<Array<String>> {
+        TODO("Not yet implemented")
+    }
+}
