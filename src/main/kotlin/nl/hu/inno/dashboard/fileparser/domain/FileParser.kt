a5938149d72a8f6b45cbe6f8b832c5765fd35d7a
Patrick <patrick.damkat@student.hu.nl>
2025-10-27 16:58:04 +0100

    add initial FileParser.kt

---
+++ src/main/kotlin/nl/hu/inno/dashboard/fileparser/domain/FileParser.kt
@@ -0,0 +1,8 @@
+package nl.hu.inno.dashboard.fileparser.domain
+
+import org.springframework.web.multipart.MultipartFile
+
+interface FileParser {
+    fun supports(file : MultipartFile) : Boolean
+    fun parse(file : MultipartFile) : List<Array<String>>
+}
