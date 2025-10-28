4a4129b2bd793c92372d9f51056d868b3701131f
Patrick <patrick.damkat@student.hu.nl>
2025-10-28 13:38:29 +0100

    improve readability of parse()

--- src/main/kotlin/nl/hu/inno/dashboard/fileparser/domain/CsvFileParser.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/fileparser/domain/CsvFileParser.kt
@@ -1,9 +1,11 @@
 package nl.hu.inno.dashboard.fileparser.domain
 
+import nl.hu.inno.dashboard.fileparser.domain.exception.CsvFileCannotBeReadException
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
 import java.io.InputStreamReader
 
 @Component
@@ -13,14 +15,17 @@ class CsvFileParser : FileParser {
     }
 
     override fun parse(file: MultipartFile): List<List<String>> {
-        val result = mutableListOf<List<String>>()
-        InputStreamReader(file.inputStream).use { reader ->
-            CSVParser.parse(reader, CSVFormat.DEFAULT).use { csvParser ->
-                for (record in csvParser) {
-                    result.add(record.map { it.trim() }.toList())
+        try {
+            file.inputStream.use { inputStream ->
+                InputStreamReader(inputStream, Charsets.UTF_8).use { reader ->
+                    val format = CSVFormat.DEFAULT.builder().setTrim(true).get()
+                    val parser = CSVParser.parse(reader, format)
+                    return parser.records.map { it.toList() }
                 }
             }
+
+        } catch (e: IOException) {
+            throw CsvFileCannotBeReadException("File cannot be read")
         }
-        return result
     }
 }
