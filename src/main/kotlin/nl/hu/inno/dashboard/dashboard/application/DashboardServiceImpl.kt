bf60f048f65be8e00152f24027681e74bb1a2260
Patrick <patrick.damkat@student.hu.nl>
2025-10-29 11:20:00 +0100

    update parseFile() in DashboardController and add initial parseAndPersistCanvasData()

--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/DashboardServiceImpl.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/DashboardServiceImpl.kt
@@ -2,12 +2,23 @@ package nl.hu.inno.dashboard.dashboard.application
 
 import nl.hu.inno.dashboard.dashboard.data.CourseRepository
 import nl.hu.inno.dashboard.dashboard.domain.Course
+import nl.hu.inno.dashboard.fileparser.application.FileParserService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
-class DashboardServiceImpl(private val courseDB: CourseRepository) : DashboardService {
+class DashboardServiceImpl(
+    private val courseDB: CourseRepository,
+    private val fileParserService: FileParserService
+) : DashboardService {
     override fun findCourseById(id: Int): Course? {
         return courseDB.findByIdOrNull(id)
+    }
+
+    override fun parseAndPersistCanvasData(file: MultipartFile) {
+        val result = fileParserService.parseFile(file)
+
+
     }
 }
--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/presentation/DashboardController.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/presentation/DashboardController.kt
@@ -3,20 +3,13 @@ package nl.hu.inno.dashboard.dashboard.presentation
 import nl.hu.inno.dashboard.dashboard.application.DashboardServiceImpl
 import nl.hu.inno.dashboard.dashboard.domain.Course
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import nl.hu.inno.dashboard.fileparser.application.FileParserService 
 
 @RestController
 @RequestMapping("/dashboard/")
 class DashboardController(
-    private val service: DashboardServiceImpl, 
-    private val fileParserService: FileParserService
+    private val service: DashboardServiceImpl
     ) {
 
     @GetMapping("/{id}")
@@ -29,10 +22,10 @@ class DashboardController(
     }
 
     @PostMapping("/parse")
-    fun parseFile(@RequestParam("file") file: MultipartFile) : ResponseEntity<List<List<String>>> {
+    fun parseFile(@RequestParam("file") file: MultipartFile) : ResponseEntity<Void> {
         return try {
-            val parsedData = fileParserService.parseFile(file)
-            ResponseEntity.ok(parsedData)
+            service.parseAndPersistCanvasData(file)
+            ResponseEntity.ok().build()
         } catch (e: Exception) {
             ResponseEntity.internalServerError().build()
         }
