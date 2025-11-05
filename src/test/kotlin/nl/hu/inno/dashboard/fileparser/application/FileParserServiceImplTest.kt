1b7977a29651cac16962e7ad9595154d6003dde8
Patrick <patrick.damkat@student.hu.nl>
2025-11-05 23:48:12 +0100

    update tests according to updated test fixture data

--- src/test/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserServiceImplTest.kt
+++ src/test/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserServiceImplTest.kt
@@ -25,23 +25,26 @@ class FileParserServiceImplTest {
 
         val actualResult = service.parseFile(csvResource)
 
-        val expectedResultSize = 5
+        val expectedResultSize = 6
         val expectedFirstRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","John Doe","john.doe@student.hu.nl","STUDENT")
         val firstResultRow = 0
-        val expectedSecondRow = listOf("9999","Test cursus - September 2010","2010-09-01 00:00:00+02:00","2011-01-30 23:59:59+01:00","John Doe","john.doe@student.hu.nl","STUDENT")
+        val expectedSecondRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","John Doe","john.doe@student.hu.nl","STUDENT")
         val secondResultRow = 1
-        val expectedThirdRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","Jane Doe","jane.doe@hu.nl","TEACHER")
+        val expectedThirdRow = listOf("9999","Test cursus - September 2010","2010-09-01 00:00:00+02:00","2011-01-30 23:59:59+01:00","John Doe","john.doe@student.hu.nl","STUDENT")
         val thirdResultRow = 2
-        val expectedFourthRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","User Null","null","STUDENT")
+        val expectedFourthRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","Jane Doe","jane.doe@hu.nl","TEACHER")
         val fourthResultRow = 3
-        val expectedFifthRow = listOf("9999","Test cursus - September 2010","2010-09-01 00:00:00+02:00","2011-01-30 23:59:59+01:00","Test User","test.user@student.hu.nl","STUDENT")
+        val expectedFifthRow = listOf("50304","Innovation Semester - September 2025","2025-09-01 00:00:00+02:00","2026-01-30 23:59:59+01:00","User Null","null","STUDENT")
         val fifthResultRow = 4
+        val expectedSixthRow = listOf("9999","Test cursus - September 2010","2010-09-01 00:00:00+02:00","2011-01-30 23:59:59+01:00","Test User","test.user@student.hu.nl","STUDENT")
+        val sixthResultRow = 5
         assertEquals(expectedResultSize, actualResult.size)
         assertEquals(expectedFirstRow, actualResult[firstResultRow])
         assertEquals(expectedSecondRow, actualResult[secondResultRow])
         assertEquals(expectedThirdRow, actualResult[thirdResultRow])
         assertEquals(expectedFourthRow, actualResult[fourthResultRow])
         assertEquals(expectedFifthRow, actualResult[fifthResultRow])
+        assertEquals(expectedSixthRow, actualResult[sixthResultRow])
     }
 
     @Test
