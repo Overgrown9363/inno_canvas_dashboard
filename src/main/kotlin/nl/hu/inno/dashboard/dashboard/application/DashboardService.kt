9261994f4b9debebb31015f0d89f861fa1884803
Patrick <patrick.damkat@student.hu.nl>
2025-11-15 16:10:52 +0100

    refactor findCourseById() into findUserById() to return a nested DTO of the user and their respective courses (in DTOs)

--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/DashboardService.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/DashboardService.kt
@@ -1,9 +1,9 @@
 package nl.hu.inno.dashboard.dashboard.application
 
-import nl.hu.inno.dashboard.dashboard.domain.Course
+import nl.hu.inno.dashboard.dashboard.application.dto.UsersDTO
 
 interface DashboardService {
-    fun findCourseById(id: Int): Course?
+    fun findUserById(id: String): UsersDTO?
     fun addUsersToCourse()
     fun updateUsersInCourse()
 }
