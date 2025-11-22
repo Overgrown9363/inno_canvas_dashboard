5cb77c2633191b4196ba600221d79440def3d323
Bilal <119631217+Futurestt@users.noreply.github.com>
2025-11-22 13:16:41 +0100

    78 token valideren in backend spring security validatie (#174)
    * Update SecurityConfig.kt
    
    * Update SecurityConfig.kt
    
    * Update SecurityConfig.kt
    
    * Update build.gradle.kts
    
    * addd env variables for now
    
    * Create UserController.kt
    
    * Update application.properties
    
    * added log in log out (WIP)
    
    * added last additions and changes
    
    * locatie veranderd
    
    ---------
    
    Co-authored-by: Turan <83589872+Broken-Lad@users.noreply.github.com>

--- frontend/canvas_dashboard/src/components/NavBar.jsx
+++ frontend/canvas_dashboard/src/components/NavBar.jsx
@@ -1,19 +1,26 @@
 import { Link } from 'react-router-dom';
 import '../css/navbar.css';
+import LogoutButton from "../components/LogoutButton";
 
 function NavBar() {
-  return (
-    <nav id="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/course-overview">Dashboard</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav id="navbar">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/course-overview">Dashboard</Link>
+                </li>
+                <li>
+                    <Link to="/token">Show Token</Link>
+                </li>
+                <li>
+                    <LogoutButton />
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
