459f8af8381a213ba57bd541f65c8e2475c0890a
Turan <83589872+Broken-Lad@users.noreply.github.com>
2025-11-22 21:56:53 +0100

    PR: 145 maken van functionaliteit in de frontend om studenten dashboard te verkrijgen (#177)
    * add DTOs for domain classes
    
    * refactor findCourseById() into findUserById() to return a nested DTO of the user and their respective courses (in DTOs)
    
    * refactor getMessage() into getUserByEmail() to retrieve a UsersDTO object
    
    * update existing POST methods with more descriptive name and update path to indicate this path is not for public use
    
    * update request paths to match updated DashboardController
    
    * make role non nullable
    
    * replace role enum dependency with String value of the role
    
    * add tests for findUserById()
    
    * update resource path and called function names to match updated controller
    
    * make user emails always lowercase
    
    * refactor getUserByEmail() into getCurrentUser(), remove PathVariable for user email and instead retrieve identity from token claims
    
    * add unit tests for getCurrentUser()
    
    * Fixed some styling issues
    
    * Fixed padding issue
    
    * Users sees own courses and also if they areoutdated or not
    
    * Added: SkeletonUserinformation for loading Userinformation now uses real data Removed dark mode css Changed: getCourse to getUserData
    
    * Removed skeletonUserInformation.css
    dark modes css
    all the console.logs
    
    * Name change
    
    ---------
    
    Co-authored-by: Patrick <patrick.damkat@student.hu.nl>

--- frontend/canvas_dashboard/src/components/NavBar.jsx
+++ frontend/canvas_dashboard/src/components/NavBar.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 import '../css/navbar.css';
 import LogoutButton from "../components/LogoutButton";
+import HuLogo from '../assets/hu-logo.svg';
 
 function NavBar() {
     return (
         <nav id="navbar">
             <ul>
+                <li>
+                    <Link to="/">
+                        <img src={HuLogo} alt="Hogeschool Utrecht logo die terug wijst naar de startpagina." />
+                    </Link>
+                </li>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
