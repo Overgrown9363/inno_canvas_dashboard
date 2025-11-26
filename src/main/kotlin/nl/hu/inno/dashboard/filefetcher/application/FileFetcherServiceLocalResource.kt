dedd2ed482fef9a33eda88e59864344c52334437
Patrick <patrick.damkat@student.hu.nl>
2025-11-26 21:37:02 +0100

    implement updated local resource implementation

--- src/main/kotlin/nl/hu/inno/dashboard/filefetcher/application/FileFetcherServiceLocalResource.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/filefetcher/application/FileFetcherServiceLocalResource.kt
@@ -1,16 +1,32 @@
 package nl.hu.inno.dashboard.filefetcher.application
 
-import org.springframework.core.io.ClassPathResource
+import nl.hu.inno.dashboard.filefetcher.domain.exception.InvalidRoleException
 import org.springframework.core.io.Resource
+import org.springframework.core.io.UrlResource
 import org.springframework.stereotype.Service
+import java.net.URI
 
 @Service
-class FileFetcherServiceLocalResource : FileFetcherService {
+class FileFetcherServiceImpl : FileFetcherService {
+    private val hostName = "http://localhost:5000"
+
     override fun fetchCsvFile(): Resource {
-        return ClassPathResource("users-01.csv")
+        val path = "${hostName}/user_data.csv"
+        return UrlResource(URI.create(path))
     }
 
     override fun fetchDashboardHtml(instanceName: String, role: String, email: String): Resource {
-        TODO("Not yet implemented")
+        val path = when (role) {
+            "ADMIN", "TEACHER" -> {
+                "${hostName}/${instanceName}/dashboard_${instanceName}/index.html"
+            }
+            "STUDENT" -> {
+                val firstPartEmail = email.substringBefore("@")
+                "${hostName}/${instanceName}/dashboard_${instanceName}/${instanceName}/students/${firstPartEmail}_index.html"
+
+            }
+            else -> throw InvalidRoleException("Role '$role' is not a valid role")
+        }
+        return UrlResource(URI.create(path))
     }
 }
