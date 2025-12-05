97d374861d1427871344f290ce4a418482816395
Broken-Lad <turan@suslu.eu>
2025-12-05 19:37:25 +0100

    Removed token page Added: Admin page and also only visible for admins

---
+++ frontend/canvas_dashboard/src/pages/admin-portal.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { getUserData } from "../api/getUserData.js";
+import CardsGrid from "../components/CardGrid";
+import UserInfo from "../components/UserInformation";
+import useAuthCheck from "../hooks/useAuthCheck";
+
+const AdminDashboard = () => {
+    useAuthCheck();
+
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function loadData() {
+            try {
+                const data = await getUserData();
+                setUserData(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadData();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error loading user data.</div>;
+
+    return (
+        <div>
+            <h1>Cursus Overzicht</h1>
+            <UserInfo data={userData} />
+            <CardsGrid courses={userData.courses} />
+        </div>
+    );
+};
+
+export default AdminDashboard;
