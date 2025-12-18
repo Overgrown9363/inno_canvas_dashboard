0920108f501c0d41c9469e8f194890b054a79b6b
Patrick <144695998+Overgrown9363@users.noreply.github.com>
2025-12-18 20:02:59 +0100

    PR: Docker volume update (#251)
    * refresh courses data with latest changes
    
    * remove static-service for test data
    
    * update pathing to be docker volume based
    
    * remove old imports and update pathing to new FileSystemResource() for production build
    
    * add shared-data volume to house files created by application and share them amongst containers
    
    * add backend service instructions for debugging, to be removed later
    
    * remove dev implementation of FileFetcherService and rely on application-dev.properties and application-prod.properties to inject the correct pathing
    
    * add dashboards for all users

--- src/main/kotlin/nl/hu/inno/dashboard/filefetcher/application/FileFetcherServiceImpl.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/filefetcher/application/FileFetcherServiceImpl.kt
@@ -4,29 +4,29 @@ import nl.hu.inno.dashboard.exception.exceptions.InvalidPathException
 import nl.hu.inno.dashboard.filefetcher.domain.HtmlPathResolver
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
+import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
-import org.springframework.core.io.UrlResource
 import org.springframework.stereotype.Service
-import java.net.URI
 
 @Service
-@Profile("prod")
 class FileFetcherServiceImpl(
-    @Value("\${filefetcher.base-url}")
-    private val baseUrl: String
+    @Value("\${volumes.path.shared-data}")
+    private val pathToSharedDataVolume: String,
+    @Value("\${volumes.path.shared-data.courses}")
+    private val coursesDirectory: String
 ) : FileFetcherService {
 
     override fun fetchCsvFile(): Resource {
-        val path = "${baseUrl}/user_data.csv"
-        return UrlResource(URI.create(path))
+        val path = "${pathToSharedDataVolume}$coursesDirectory/user_data.csv"
+        return FileSystemResource(path)
     }
 
     override fun fetchDashboardHtml(email: String, role: String, courseCode: String, instanceName: String, relativeRequestPath: String): Resource {
-        val baseUrlWithInstance = "$baseUrl/$courseCode/$instanceName/dashboard"
+        val baseUrlWithInstance = "$pathToSharedDataVolume$coursesDirectory/$courseCode/$instanceName/dashboard"
         val resolvedPath = HtmlPathResolver.resolvePath(email, role, instanceName, relativeRequestPath)
         val fullPath = "$baseUrlWithInstance/$resolvedPath"
 
-        val resource = UrlResource(URI.create(fullPath))
+        val resource = FileSystemResource(fullPath)
         if (!resource.exists()) {
             throw InvalidPathException("Path $resolvedPath did not lead to an existing resource")
         }
