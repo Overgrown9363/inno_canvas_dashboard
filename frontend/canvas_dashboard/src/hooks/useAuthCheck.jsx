ea47af2027ff4b07cdc6af6f6a2b88e04640731b
Broken-Lad <turan@suslu.eu>
2026-01-08 15:55:23 +0100

    fixed eslint errors

--- frontend/canvas_dashboard/src/hooks/useAuthCheck.jsx
+++ frontend/canvas_dashboard/src/hooks/useAuthCheck.jsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const VITE_AUTH_URL = import.meta.env.VITE_AUTH_URL;
 
 function redirectToLogin() {
     const returnTo = encodeURIComponent(window.location.href);
-    fetch("/api/v1/security/redirect", {
-        method: "POST",
+    fetch('/api/v1/security/redirect', {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        credentials: "include",
-        body: "url=" + returnTo
+        credentials: 'include',
+        body: 'url=' + returnTo
     }).finally(() => {
         window.location.href = VITE_AUTH_URL;
     });
@@ -18,7 +18,7 @@ function redirectToLogin() {
 
 function useAuthCheck() {
     useEffect(() => {
-        fetch("/api/v1/security/users/me", { credentials: "include" })
+        fetch('/api/v1/security/users/me', { credentials: 'include' })
             .then((response) => {
                 if (response.status === 401 || response.status === 403) {
                     redirectToLogin();
