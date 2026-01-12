b37152de27ff3b9d4a2399277143405e06ebb360
Patrick <patrick.damkat@student.hu.nl>
2026-01-12 16:51:44 +0100

    update service to use postToPythonEnvironment()

--- src/main/kotlin/nl/hu/inno/dashboard/pythongateway/application/PythonGatewayServiceImpl.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/pythongateway/application/PythonGatewayServiceImpl.kt
@@ -3,19 +3,20 @@ package nl.hu.inno.dashboard.pythongateway.application
 import nl.hu.inno.dashboard.dashboard.application.DashboardService
 import nl.hu.inno.dashboard.exception.exceptions.InvalidPythonEnvironmentException
 import nl.hu.inno.dashboard.pythongateway.domain.PythonEnvironment
+import nl.hu.inno.dashboard.pythongateway.domain.PythonRestClient
 import org.springframework.stereotype.Service
 
 @Service
 class PythonGatewayServiceImpl(
-    private val dashboardService: DashboardService
+    private val dashboardService: DashboardService,
+    private val pythonRestClient: PythonRestClient
 ) : PythonGatewayService {
 
     override fun startPythonScript(email: String, environment: String) {
         dashboardService.verifyUserIsAdminOrSuperAdmin(email)
 
         val requestedEnvironment = parsePythonEnvironment(environment)
-
-        TODO("Create rest template in domain layer that makes a POST to Python env 2 or 3 depending on the requested environment")
+        pythonRestClient.postToPythonEnvironment(requestedEnvironment)
     }
 
     private fun parsePythonEnvironment(environment: String): PythonEnvironment =
